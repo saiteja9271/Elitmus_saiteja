398b0ccebf4aa01286f3154f7332405154429d89
saiteja9271 <82574494+saiteja9271@users.noreply.github.com>
2023-05-03 04:06:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,14 +15,14 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).then((
   });
 app.use(express.json());
 
-app.use(cors(
-  {
-      origin: [
-        "http://localhost:3000",
-      ],
-      credentials: true,
-    }
-))
+app.use(cors())
+//   {
+//       origin: [
+//         "http://localhost:3000",
+//       ],
+//       credentials: true,
+//     }
+// ))
 app.use(route);
 
 app.listen(5000, () => {
